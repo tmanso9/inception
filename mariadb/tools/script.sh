7ab250aa462d5e4d344f0683c4fb495d97360358
tmanso9 <tmanso92@gmail.com>
2023-06-16 19:48:39 +0100

    trying mariadb, still not working

--- mariadb/tools/script.sh
+++ mariadb/tools/script.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
+MYSQL_DB_NAME="testDB"
+MYSQL_DB_PASSWORD="bla"
+MYSQL_DB_USER="touteiro"
+MYSQL_DB_ROOT_PASSWORD="bla"
+
 service mysql start
+
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_DB_USER'@'%' IDENTIFIED BY '$MYSQL_DB_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DB_NAME.* to '$MYSQL_DB_USER'@'%' ;" >> db1.sql
+echo " ALTER USER root@localhost IDENTIFIED BY '$MYSQL_DB_ROOT_PASSWORD' ;" >> db1.sql
+echo "FLUSH PRIVILEGES ;" >> db1.sql
+
+db1.sql < mysql
+
+mysqld
+
+# mysql -sfu root << EOS
+# -- set root password
+# UPDATE mysql.user SET Password=PASSWORD('bla') WHERE User='root';
+# -- delete anonymous users
+# DELETE FROM mysql.user WHERE User='';
+# -- delete remote root capabilities
+# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '0.0.0.0', '::1');
+# -- drop database 'test'
+# DROP DATABASE IF EXISTS test;
+# -- also make sure there are lingering permissions to it
+# DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+# -- make changes immediately
+# FLUSH PRIVILEGES;
+# EOS
